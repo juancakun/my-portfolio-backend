4f12e0ea4850585f214676d85072be2cdeb324a7
juancakun <juancarlos.hdz.montero@gmail.com>
2026-08-25 20:50:00 -0600

    feat(model): create core entities and static assets for portfolio

---
+++ src/main/java/portfolio/model/PersonalInfo.java
@@ -0,0 +1,26 @@
+package portfolio.model;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PersonalInfo {
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacio.")
+
+    private String firstName;
+    private String lastName;
+    private String title;
+    private String profileDescription;
+    private String profileImageUrl;
+    private Integer yearsOfExperience;
+    private String email;
+    private String phone;
+    private String linkedinUrl;
+    private String githubUrl;
+}
---
+++ src/main/java/portfolio/model/Skill.java
@@ -0,0 +1,16 @@
+package portfolio.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Skill {
+    private Long id;
+    private String name;
+    private int levelPercentage;
+    private String iconClass;
+    private Long personalInfoId;
+}
